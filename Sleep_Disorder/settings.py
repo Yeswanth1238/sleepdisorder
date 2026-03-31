60729315a892db4e20d52157b5d1a36da1a1fb39
Yeswanth1238 <yeswanthsaiyeswanth@gmail.com>
2026-03-31 12:02:39 +0530

    Add requirements.txt and production settings

--- Sleep_Disorder/settings.py
+++ Sleep_Disorder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-27atmk#ynr16mrw8uyl!p3ju)0x(-v*c@3#i8u@c^dq*z2zgfc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,8 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
